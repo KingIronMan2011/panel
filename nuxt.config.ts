958f53e7a0a63a2d222001533d7f72af528b116c
26bz <26bz@proton.me>
2025-11-18 16:33:21 -0500

    fix: declare nuxt-charts module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/ui', 'nuxt-qrcode', '@nuxt/eslint', '@sidebase/nuxt-auth', 'nuxt-security', '@nuxt/test-utils/module', '@nuxt/hints'],
+  modules: [
+    '@nuxt/ui',
+    'nuxt-qrcode',
+    '@nuxt/eslint',
+    '@sidebase/nuxt-auth',
+    'nuxt-security',
+    '@nuxt/test-utils/module',
+    '@nuxt/hints',
+    'nuxt-charts',
+  ],
 
   auth: {
     origin: authOrigin,
