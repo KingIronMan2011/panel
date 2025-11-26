2454c5c98a1ed38d8850d456ff52c9410dd05d00
26bz <26bz@proton.me>
2025-11-25 20:01:07 -0500

    chore: update database URL in drizzle conf

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "drizzle-kit";
+
 export default defineConfig({
   dialect: "sqlite",
   schema: "./server/database/schema.ts",
   out: "./server/database/migrations",
   dbCredentials: {
-    url: process.env.DRIZZLE_DATABASE_URL || "file:./server/database/dev.db",
-  }
+    url: process.env.DRIZZLE_DATABASE_URL || "file:./data/XyraPanel.sqlite",
+  },
 });
